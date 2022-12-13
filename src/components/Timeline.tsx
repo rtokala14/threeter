4a642d6d75e1b3440df680ec5e305f8259f178bf
Rohit Tokala <rtokala@purdue.edu>
2022-12-13 15:25:03 -0500

    Fix trpc reading undefined error

--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -1,8 +1,11 @@
+import { signOut, useSession } from "next-auth/react";
 import { CreateTweet } from "./CreateTweet";
 
 export function Timeline() {
+  const { data: session } = useSession();
   return (
     <div>
+      {session ? <button onClick={() => signOut()}>Log Out</button> : <></>}
       <CreateTweet />
     </div>
   );
