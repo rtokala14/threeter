045011e0ba00c611af4f9c782a52589b9fbc76a4
Rohit Tokala <rtokala@purdue.edu>
2022-12-13 16:17:57 -0500

    Add timeline

--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -1,12 +1,88 @@
+import dayjs from "dayjs";
 import { signOut, useSession } from "next-auth/react";
+import Image from "next/image";
+import { RouterOutputs, trpc } from "../utils/trpc";
 import { CreateTweet } from "./CreateTweet";
+import relativeTime from "dayjs/plugin/relativeTime";
+import updateLocal from "dayjs/plugin/updateLocale";
+
+dayjs.extend(relativeTime);
+dayjs.extend(updateLocal);
+
+dayjs.updateLocale("en", {
+  relativeTime: {
+    future: "in %s",
+    past: "%s",
+    s: "1m",
+    m: "1m",
+    mm: "%dm",
+    h: "1h",
+    hh: "%dh",
+    d: "1d",
+    dd: "%dd",
+    M: "1M",
+    MM: "%dM",
+    y: "1y",
+    yy: "%dy",
+  },
+});
+
+function Tweet({
+  tweet,
+}: {
+  tweet: RouterOutputs["tweet"]["timeline"]["tweets"][number];
+}) {
+  return (
+    <div className="mb-4 border-b-2 border-gray-500">
+      <div className="flex p-2">
+        {tweet.author.image && (
+          <Image
+            src={tweet.author.image}
+            alt={`${tweet.author.name} profile picture`}
+            width={48}
+            height={48}
+            className="rounded-full"
+          />
+        )}
+
+        <div className="ml-2">
+          <div className="flex items-center">
+            <p className="font-bold">{tweet.author.name}</p>
+            <p className="text-sm text-gray-400">
+              {"  -  "}
+              {dayjs(tweet.createdAt).fromNow()}
+            </p>
+          </div>
+          <div>{tweet.text}</div>
+        </div>
+      </div>
+    </div>
+  );
+}
 
 export function Timeline() {
   const { data: session } = useSession();
+
+  const { data, hasNextPage, fetchNextPage, isFetched } =
+    trpc.tweet.timeline.useInfiniteQuery(
+      {
+        limit: 10,
+      },
+      {
+        getNextPageParam: (lastPage) => lastPage.nextCursor,
+      }
+    );
+
   return (
     <div>
-      {session ? <button onClick={() => signOut()}>Log Out</button> : <></>}
+      {/* {session ? <button onClick={() => signOut()}>Log Out</button> : <></>} */}
       <CreateTweet />
+      {/* {JSON.stringify(data)} */}
+      <div className="border-l-2 border-r-2 border-t-2 border-gray-500">
+        {data?.tweets.map((tweet) => {
+          return <Tweet key={tweet.id} tweet={tweet} />;
+        })}
+      </div>
     </div>
   );
 }
