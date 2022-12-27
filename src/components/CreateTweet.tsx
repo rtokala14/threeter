170de79f113d1ed342020f2ea6f9ed241a03a8a2
Rohit Tokala <rtokala@purdue.edu>
2022-12-27 13:44:46 -0500

    Add scrollbar to show tweet length

--- src/components/CreateTweet.tsx
+++ src/components/CreateTweet.tsx
@@ -4,6 +4,9 @@ import { trpc } from "../utils/trpc";
 import Image from "next/image";
 import { useSession } from "next-auth/react";
 import Link from "next/link";
+import TextareaAutosize from "react-textarea-autosize";
+import { CircularProgressbar } from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
 
 export const tweetSchema = object({
   text: string({
@@ -53,7 +56,7 @@ export function CreateTweet() {
           onSubmit={handleSubmit}
           className="mb-4 flex w-full flex-col rounded-md  p-4"
         >
-          <div className=" flex flex-row items-center justify-between">
+          <div className=" flex flex-row justify-between">
             <Link href={`/${session.user?.name}`}>
               <Image
                 src={session.user!.image!}
@@ -64,16 +67,64 @@ export function CreateTweet() {
               />
             </Link>
 
-            <textarea
-              className="w-full p-4 shadow"
+            <TextareaAutosize
+              className="w-full resize-none p-4 shadow"
               value={text}
               onChange={(e) => setText(e.target.value)}
               placeholder={"What's Happening?"}
+              minLength={10}
+              maxLength={280}
+              minRows={4}
+              maxRows={8}
             />
           </div>
-          <div className="mt-4 flex justify-end">
+          <div className="mt-4 flex items-center justify-end">
+            {text.length > 0 ? (
+              text.length === 280 ? (
+                <CircularProgressbar
+                  value={(text.length / 280) * 100}
+                  className=" h-10 w-10 "
+                  styles={{
+                    root: {
+                      width: "13%",
+                    },
+                    path: {
+                      stroke: "red",
+                    },
+                  }}
+                />
+              ) : text.length > 200 ? (
+                <CircularProgressbar
+                  value={(text.length / 280) * 100}
+                  className=" h-10 w-10 "
+                  styles={{
+                    root: {
+                      width: "13%",
+                    },
+                    path: {
+                      stroke: "orange",
+                    },
+                  }}
+                />
+              ) : (
+                <CircularProgressbar
+                  value={(text.length / 280) * 100}
+                  className=" h-10 w-10 "
+                  styles={{
+                    root: {
+                      width: "13%",
+                    },
+                    path: {
+                      stroke: "blue",
+                    },
+                  }}
+                />
+              )
+            ) : (
+              <></>
+            )}
             <button
-              className="rounded-md bg-primary px-4 py-2 text-white"
+              className="rounded-md bg-primary px-5 py-2 text-white"
               type="submit"
             >
               Threet
